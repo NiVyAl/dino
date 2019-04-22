9b5fa4a5ec5bbefc7581ed92cc89a1cf96db4dfd
nick <nikitavisoc@gmail.com>
2019-04-22 22:53:28 +0300

    сделал кактус

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -14,23 +14,12 @@ var animationInterval = setInterval(function(){
 }, 100); 
 
 
-/* */
+/* прыжок */
 
 var jumpHeight = 50;
 var isKeyup = false;
 document.addEventListener('keydown', function(evt) {
   if ((evt.keyCode === 32) && (isKeyup == false)) {
-    /*isKeyup = true;
-    var jumpInterval = setInterval(function(){
-      if (isKeyup) {
-        dino.classList.add("dino--jump");
-        dino.style.transform = 'translateY(' + -jumpHeight + 'px)';
-        console.log(jumpHeight);
-        if (jumpHeight <  100) {
-          jumpHeight = jumpHeight + 5;    
-        }
-      }
-    }, 1000); */
     isKeyup = true;
     dino.classList.add("dino--jump");
     dino.style.transform = 'translateY(-100px)';
@@ -49,78 +38,41 @@ document.addEventListener('keydown', function(evt) {
     }, 600);
   }
 });
-/*
-document.addEventListener('keyup', function(evt) {
-  if (evt.keyCode === 32) {
-    console.log("отпустил");
-    
-    isKeyup = false;
-    jumpHeight = 50;
-    dino.classList.remove("dino--jump");
-    dino.style.transform = 'translateY(0)';
-  }
-}); */
 
 
 /* cloud */
 
 var cloud = document.querySelector(".cloud-container__item");
-
-/*var animationPause = function() {
-  var l = new Date();
-  var c;
-  var diff;
-  while(1) {
-    c = new Date();
-    diff = c - l;
-    if (diff > 1000) {
-      break;
-    }
-  }
-} 
-*/
+var cactus = document.querySelector(".cactus");
 
 cloud.classList.remove("cloud-container__item--move");
-var a = 0;
-var b = 0;
-var dateDecorationRemove = 0;
-var decorationMove = function(object) {
 
-  object.style.top = Math.floor(Math.random() * (100 - 1) ) + 1 + 'px';
-  object.classList.add("cloud-container__item--move");
+var decorationMove = function(object) {
+  if (object == cloud) {
+    var classMove = "cloud-container__item--move"; 
+    var moveTime = 10000;
+    
+    object.style.top = Math.floor(Math.random() * (100 - 1) ) + 1 + 'px';
+  };
+  if (object == cactus) {
+    var classMove = "cactus--move";
+    var moveTime = 3000;
+  };
+  
+  object.classList.add(classMove);
   
   setInterval(function() {
-    object.classList.remove("cloud-container__item--move");
-    //console.log("remove");
-    a = a + 10000;
-    //console.log("a: " + a);
-    //console.log("b: " + b);
-    
-    dateDecorationRemove = Date.now();
+    object.classList.remove(classMove);
     
     setTimeout(function() {
-      object.style.top = Math.floor(Math.random() * (100 - 1) ) + 1 + 'px';
-      console.log("timeOut");
-      object.classList.add("cloud-container__item--move");
+      if (object == cloud) {
+        object.style.top = Math.floor(Math.random() * (100 - 1) ) + 1 + 'px';  
+      };
+      
+      object.classList.add(classMove);
     }, 20);
-  }, 10000);
-  /*
-  setTimeout(function() {
-      //console.log(Date.now() - dateDecorationRemove);
-      if ( (7 < (Date.now() - dateDecorationRemove)) && ((Date.now() - dateDecorationRemove) < 20)) {
-          //console.log("разница времени " + (Date.now() - dateDecorationRemove) );
-          //console.log("remove time " + a);
-          //console.log("add time " + b);
-
-          object.style.top = Math.floor(Math.random() * (100 - 1) ) + 1 + 'px';
-
-          object.classList.add("cloud-container__item--move");
-          //console.log("add");    
-      }
-
-        //b = b + 10;
-        //console.log("b: " + b);
-    }, 10); */
-} 
+  }, moveTime);
+}; 
 
 decorationMove(cloud);
+decorationMove(cactus);
